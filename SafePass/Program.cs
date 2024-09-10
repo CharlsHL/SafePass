1ec204f96f307259975a71f9083914da05c03a19
Charlie Ermeti <Carlosermeti@hotmail.com>
2024-09-10 12:02:35 -0300

    Agregado de icono

--- SafePass/Program.cs
+++ SafePass/Program.cs
@@ -29,7 +29,7 @@ class Program
 
             switch (input.ToLower())
             {
-                case "add":
+                case "add": 
                     ContextoDB.AddPassword(DatabaseFile);
                     break;
                 case "list":
